cd47c343361d05dc4e4004a93f2b81aa90d20fb6
= <kadabra777@gmail.com>
2023-03-09 23:53:54 +0200

    added folder structure

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(1 == 1);
+    }
+
+}
