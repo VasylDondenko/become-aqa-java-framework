4c48e91b0993189c4f9fa7a6a67ab563a75cb834
Vasyl Dondenko <Vasyl.Dondenko@gmail.com>
2023-03-14 20:17:54 +0200

    User tests added

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,36 @@
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import users.User;
+
+import java.util.Objects;
 
 public class FirstTest {
+
+    private User user;
+
+    @BeforeMethod
+    public void setUp() {
+        user = new User(52);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (Objects.nonNull(user)) {
+            user = null;
+        }
+    }
+
+    @Test
+    public void isUser52() {
+        Assert.assertEquals(user.getAge(), 52);
+    }
+
+    @Test
+    public void isUser50() {
+        Assert.assertEquals(user.getAge(), 50);
+    }
 
     @Test
     public void firstTest() {
