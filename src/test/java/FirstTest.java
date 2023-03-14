cd135d7f8bffec4b16dc145a2902103e4dade6b5
Vasyl Dondenko <Vasyl.Dondenko@gmail.com>
2023-03-14 20:23:46 +0200

    junit4 added User class and tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,36 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import users.User;
+
+import java.util.Objects;
 
 public class FirstTest {
+
+    private User user;
+
+    @Before
+    public void setUp() {
+        user = new User(52);
+    }
+
+    @After
+    public void tearDown() {
+        if (Objects.nonNull(user)) {
+            user = null;
+        }
+    }
+
+    @Test
+    public void isUser52() {
+        Assert.assertEquals(user.getAge(), 52);
+    }
+
+    @Test
+    public void isUser50() {
+        Assert.assertEquals(user.getAge(), 50);
+    }
 
     @Test
     public void firstTest() {
