83ed94fc53c48494d50e86a52e2cc8409c82980f
Vasyl Dondenko <Vasyl.Dondenko@gmail.com>
2023-03-14 21:15:22 +0200

    added User class and tests

---
+++ src/main/java/users/User.java
@@ -0,0 +1,15 @@
+package users;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(int age) {
+        name = "Username";
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,36 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import users.User;
+
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstTest {
+    private User user;
 
+    @BeforeEach
+    public void setUp() {
+        user = new User(52);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (Objects.nonNull(user)) {
+            user = null;
+        }
+    }
+
+    @Test
+    public void testIsUser52() {
+        assertEquals(user.getAge(), 52);
+    }
+
+    @Test
+    public void testIsUser50() {
+        assertEquals(user.getAge(), 50);
+    }
     @Test
     public void firstTest() {
         assertEquals(2, 1+1);
